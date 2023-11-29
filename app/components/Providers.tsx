8a0282aaea5d76e36be2b3770fd9a76dae24ffb2
Moudi <mjaderi97@gmail.com>
2023-11-29 13:56:41 +1100

    Updated success/error url for stripe to deployed url

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://nyla-ecommerce-v3.vercel.app/stripe/success"
+      cancelUrl="https://nyla-ecommerce-v3.vercel.app/error"
       currency="AUD"
       billingAddressCollection={true}
       shouldPersist={true}
